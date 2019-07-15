f237b347bd7b8866d9b8e8177752887a789bc8aa
Juan Palma <juan.palma@me.com>
2019-07-15 06:06:52 -0500

    pruebas librería dotenv

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+	require('vendor/autoload.php');
+	$dotenv = new Dotenv\Dotenv('../');
+	$dotenv->load();
+	
 	//echo($_ENV["PRUEBA"]);
 	echo (getenv('PRUEBA'));
+	//echo (getenv('HOME'));
 ?>
