fc572875d707eddb280484a56d4eba61b1da1101
Juan Palma <juan.palma@me.com>
2019-07-25 03:30:37 -0500

    pruebas

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -32,6 +32,8 @@ class Login extends CI_Controller {
 			if( $post['username'] !== '' && $post['password'] !== '' ){
 				$result = $this->admin_modal->loginValid($post['username'], $post['password']);
 				if(isset($result) && count($result) > 0){					
+					print_r($result);
+					echo($this->ida_protect->decrypt($result[0]->user_pass));
 					if($post['username'] === $result[0]->user_user && $post['password'] === $this->ida_protect->decrypt($result[0]->user_pass)){
 						$userData = array(
 							'userID' => $result[0]->id_user,
@@ -70,7 +72,7 @@ class Login extends CI_Controller {
 			
 			$this->session->set_flashdata($this->varFlash.'Success', $this->success);
 			$this->session->set_flashdata($this->varFlash.'Error', $this->error);
-			redirect($redir);
+			//redirect($redir);
 		}
 		
 		
