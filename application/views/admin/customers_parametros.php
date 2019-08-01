ec8a0e3f628f18df32704b84957a2e7b315bd587
Juan Palma <juan.palma@me.com>
2019-07-31 20:04:10 -0500

    Prueba envió  Mail ajuste 5

--- application/views/admin/customers_parametros.php
+++ application/views/admin/customers_parametros.php
@@ -14,19 +14,21 @@
 	$idaMail_data['origen_nombre'] = 'Contacto - INMOTION';
 	$idaMail_data['origen_mail'] = 'info@inmotion.com';
 	$idaMail_data['reply_nombre'] = 'Sistema - INMOTION';
-	$idaMail_data['reply_mail'] = 'informes@idalibre.com';
+	$idaMail_data['reply_mail'] = 'info@inmotion.com';
 	$idaMail_data['organizacion'] = 'INMOTION';
 	$idaMail_data['asunto'] = 'Nuevo contacto desde el formulario de tu sitio web.';
 	
 	$idaMail_data['priority'] = 3;
 	$idaMail_data['encoding'] = 'quoted-printable';
 	
+/*
 	$idaMail_data['host'] = 'smtp.gmail.com';
 	$idaMail_data['port'] = 587;
 	$idaMail_data['username'] = 'info@inmotion.com';
 	$idaMail_data['password'] = 'pass';
 	
 	$idaMail_data['username'] = '';
+*/
 	
 	
 	$idaMail_data['texto_plano'] = '
